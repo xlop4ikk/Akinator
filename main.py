13525ffa7d6c6711a515b973786a50a20186ce68
xlop4ikk <bolshakovdan621@mail.ru>
2023-06-14 23:09:10 +0500

    Add files

---
+++ main.py
@@ -0,0 +1,54 @@
+# -----------------------------------------------------------------------------------------------------------
+# Akinator
+# -----------------------------------------------------------------------------------------------------------
+
+class TreeNode:
+
+    # Инициализатор для создания узлов и вопросов в бинарном дереве
+    def __init__(self, question):
+        self.question = question
+        self.left = None
+        self.right = None
+
+class QAkinator:
+
+    # Метод для задания вопроса
+    def yes(ques):
+        while True:
+            ans = input(ques)
+            if ans == 'Да':
+                return True
+            if ans == 'Нет':
+                return False
+
+# Задание данных дерева
+tree = TreeNode("человек?")
+tree.left = TreeNode("животное?")
+tree.right = TreeNode("ПРЕЗИДЕНТ?")
+tree.left.left = TreeNode("МЫЛО?")
+tree.left.right = TreeNode("летает?")
+tree.left.right.left = TreeNode("СОБАКА?")
+tree.left.right.right = TreeNode("ПТИЦА?")
+
+# Здесь рофлан в том, что происходит бесконечный цикл до момента, пока не кончатся узлы в бинарном дереве.
+# Как только они кончаются, акинатор предлагает свой ответ
+def main():
+    while True:
+        p = tree
+        while p.left != None:
+            if QAkinator.yes("Ваш персонаж " + p.question + " "):
+                p = p.right
+            else:
+                p = p.left
+        if QAkinator.yes("Ваш персонаж " + p.question + " "):
+            print("Ура! Я отгадал, я молодец!")
+            break
+        animal = input("Какого персонажа вы загадали? ")
+        question = input("Какой вопрос различает персонажи КОШКА от СОБАКА? ")
+        p.left = TreeNode(p.question)
+        p.right = TreeNode(animal)
+        p.question = question
+        if not QAkinator.yes("Если персонаж кошка, то ответом на это вопрос будет Да или Нет? "):
+            (p.right, p.left) = (p.left, p.right)
+
+if __name__ == "__main__": main()
